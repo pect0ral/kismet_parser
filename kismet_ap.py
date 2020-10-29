c84acde3fc5ed662d7e293f0063a6d71fe7a6494
Mike Piekarski <mjpiekarski@gmail.com>
2020-10-28 23:43:24 -0400

    Added an AP Parser with Signal Strength. Easy for parsing with Awk.

--- kismet_ap.py
+++ kismet_ap.py
@@ -6,8 +6,10 @@ with open(sys.argv[1]) as f:
     k_dict = json.load(f)
 print("Chan\tBSSID\tType\tESSID")
 for d in k_dict:
-    print("{}\t{}\t{}\t{}".format(d['kismet.device.base.channel'],
+  if d['kismet.device.base.type'] == 'Wi-Fi AP':
+    print("{}\t{}\t{}\t{}\t{}".format(d['kismet.device.base.channel'],
     d['kismet.device.base.macaddr'],
     d['kismet.device.base.type'],
     d['kismet.device.base.name'],
+    d['kismet.device.base.signal']['kismet.common.signal.last_signal']
     ))
